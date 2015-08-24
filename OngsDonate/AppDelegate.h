d5fd76e6c46b1ac7cabb3cd73ac070f478670c7c
Bruno Isola Reginato <brunoisola@ciandt.com>
2015-08-24 08:54:55 -0300

    Pushing initial files

---
+++ OngsDonate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OngsDonate
+//
+//  Created by Bruno Isola Reginato on 8/24/15.
+//  Copyright (c) 2015 ong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OngsDonate/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  OngsDonate
+//
+//  Created by Bruno Isola Reginato on 8/24/15.
+//  Copyright (c) 2015 ong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
